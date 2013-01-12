723e1ebc0b76795b18bb10d7346648e503ee5174
Pedro Algarvio <pedro@algarvio.me>
2013-01-12 15:53:53 +0000

    Adapt the hooks handler to the new approach.

--- saltci/web/views/hooks.py
+++ saltci/web/views/hooks.py
@@ -14,7 +14,7 @@ import json
 import pprint
 import logging
 from saltci.web.application import *
-from saltci.database.models import Account
+from saltci.database.models import Account, Repository
 
 log = logging.getLogger(__name__)
 
@@ -29,20 +29,38 @@ hooks = Blueprint('hooks', __name__, url_prefix='/hooks')
 @hooks.route('/push/<login>/<reponame>', methods=('POST',), defaults={'organization': None})
 @hooks.route('/push/<login>/<organization>/<reponame>', methods=('POST',))
 def push(login=None, organization=None, reponame=None):
-    if request.remote_addr not in app.config.get('GITHUB_PAYLOAD_IPS', ()):
+    try:
+        payload = request.json
+        if payload is None:
+            log.warning(
+                'Got a malformed request from {0!r}. Request arguments: {1!r}  Request Values: '
+                '{2!r}'.format(
+                    request.remote_addr, request.args, request.values
+                )
+            )
+            abort(404)
+    except Exception, err:
         log.warning(
-            'Got a hooks push request from an invalid address({0}). Request Values: {1}'.format(
-                request.remote_addr, request.values
+            'Got a malformed request from {0!r} which threw an exeption. Exception: {1!r}'.format(
+                request.remote_addr, err
             )
         )
-        abort(401)
+        abort(404)
+
+    if request.remote_addr not in app.config.get('GITHUB_PAYLOAD_IPS', ()):
+        log.warning(
+            'Got a hooks push request from an invalid address({0}). Payload: {1}'.format(
+                request.remote_addr, pprint.pformat(payload, indent=2)
+            )
+        )
+        abort(404)
 
     account = Account.query.get(login)
     if account is None:
         log.warning(
             'Got a hooks push request from {0} with an invalid login({1}). '
             'Request Values: {2}'.format(
-                request.remote_addr, login, pprint.pformat(request.values, indent=2)
+                request.remote_addr, login, pprint.pformat(payload, indent=2)
             )
         )
         abort(401)
@@ -51,65 +69,76 @@ def push(login=None, organization=None, reponame=None):
         org = Organization.query.get(organization)
         if org is None:
             log.warning(
-                'Got a hooks pull request from {0} with an invalid organization({1}). '
-                'Request Values: {2}'.format(
-                    request.remote_addr, organization, pprint.pformat(request.values, indent=2)
+                'Got a hooks push request from {0} with an invalid organization({1}). '
+                'Payload: {2}'.format(
+                    request.remote_addr, organization, pprint.pformat(payload, indent=2)
                 )
             )
-            abort(401)
+            abort(404)
         elif org not in account.organizations:
             log.warning(
-                'Got a hooks pull request from {0} with an organization the user does not '
-                'belong to({1}). Request Values: {2}'.format(
-                    request.remote_addr, organization, pprint.pformat(request.values, indent=2)
+                'Got a hooks push request from {0} with an organization the user does not '
+                'belong to({1}). Payload: {2}'.format(
+                    request.remote_addr, organization, pprint.pformat(payload, indent=2)
                 )
             )
-            abort(401)
+            abort(404)
 
-    repository = account.managed_repositories.filter(models.Repository.name == 'salt').first()
+    repository = account.managed_repositories.filter(Repository.name == reponame).first()
     if repository is None:
         log.warning(
             'Got a hooks push request from {0} with an invalid repository({1}). '
-            'Request Values: {2}'.format(
-                request.remote_addr, reponame, pprint.pformat(request.values, indent=2)
+            'Payload: {2}'.format(
+                request.remote_addr, reponame, pprint.pformat(payload, indent=2)
             )
         )
-        abort(401)
+        abort(404)
 
     # Just log the payload information
-    try:
-        log.debug(
-            'Incoming GitHub Push payload:\n{0}'.format(
-                pprint.pformat(
-                    json.loads(request.form.get('payload')),
-                    indent=2
-                )
-            )
+    log.debug(
+        'Incoming GitHub Push payload:\n{0}'.format(
+            pprint.pformat(payload, indent=2)
         )
-    except:
-        log.debug('RAW DATA: {0}'.format(request.data))
-        log.debug('Args: {0}'.format(request.args))
-        log.debug('Values: {0}'.format(request.values))
+    )
+
     return jsonify({'result': 'OK'})
 
 
 @hooks.route('/pull/<login>/<reponame>', methods=('POST',), defaults={'organization': None})
 @hooks.route('/pull/<login>/<organization>/<reponame>', methods=('POST',))
 def pull(login=None, organization=None, reponame=None):
-    if request.remote_addr not in app.config.get('GITHUB_PAYLOAD_IPS', ()):
+    try:
+        payload = request.json
+        if payload is None:
+            log.warning(
+                'Got a malformed request from {0!r}. Request arguments: {1!r}  Request Values: '
+                '{2!r}'.format(
+                    request.remote_addr, request.args, request.values
+                )
+            )
+            abort(404)
+    except Exception, err:
         log.warning(
-            'Got a hooks pull request from an invalid address({0}). Request Values: {1}'.format(
-                request.remote_addr, request.values
+            'Got a malformed request from {0!r} which threw an exeption. Exception: {1!r}'.format(
+                request.remote_addr, err
             )
         )
-        abort(401)
+        abort(404)
+
+    if request.remote_addr not in app.config.get('GITHUB_PAYLOAD_IPS', ()):
+        log.warning(
+            'Got a hooks pull request from an invalid address({0}). Payload: {1}'.format(
+                request.remote_addr, pprint.pformat(payload, indent=2)
+            )
+        )
+        abort(404)
 
     account = Account.query.get(login)
     if account is None:
         log.warning(
             'Got a hooks pull request from {0} with an invalid login({1}). '
             'Request Values: {2}'.format(
-                request.remote_addr, login, pprint.pformat(request.values, indent=2)
+                request.remote_addr, login, pprint.pformat(payload, indent=2)
             )
         )
         abort(401)
@@ -119,43 +148,36 @@ def pull(login=None, organization=None, reponame=None):
         if org is None:
             log.warning(
                 'Got a hooks pull request from {0} with an invalid organization({1}). '
-                'Request Values: {2}'.format(
-                    request.remote_addr, organization, pprint.pformat(request.values, indent=2)
+                'Payload: {2}'.format(
+                    request.remote_addr, organization, pprint.pformat(payload, indent=2)
                 )
             )
-            abort(401)
+            abort(404)
         elif org not in account.organizations:
             log.warning(
                 'Got a hooks pull request from {0} with an organization the user does not '
-                'belong to({1}). Request Values: {2}'.format(
-                    request.remote_addr, organization, pprint.pformat(request.values, indent=2)
+                'belong to({1}). Payload: {2}'.format(
+                    request.remote_addr, organization, pprint.pformat(payload, indent=2)
                 )
             )
-            abort(401)
+            abort(404)
 
-    repository = account.managed_repositories.filter(models.Repository.name == 'salt').first()
+    repository = account.managed_repositories.filter(Repository.name == reponame).first()
     if repository is None:
         log.warning(
             'Got a hooks pull request from {0} with an invalid repository({1}). '
-            'Request Values: {2}'.format(
-                request.remote_addr, reponame, pprint.pformat(request.values, indent=2)
+            'Payload: {2}'.format(
+                request.remote_addr, reponame, pprint.pformat(payload, indent=2)
             )
         )
-        abort(401)
+        abort(404)
 
     # Just log the payload information
-    try:
-        log.debug(
-            'Incoming GitHub Pull payload:\n{0}'.format(
-                pprint.pformat(
-                    json.loads(request.form.get('payload')),
-                    indent=2
-                )
-            )
+    log.debug(
+        'Incoming GitHub Pull payload:\n{0}'.format(
+            pprint.pformat(payload, indent=2)
         )
-    except:
-        log.debug('RAW DATA: {0}'.format(request.data))
-        log.debug('Args: {0}'.format(request.args))
-        log.debug('Values: {0}'.format(request.values))
+    )
+
     return jsonify({'result': 'OK'})
 # <---- Views ------------------------------------------------------------------------------------
